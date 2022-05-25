cc7d66e9c9ac162ae35063c9cb0efbde0ee76e4d
Ross Godfrey <ross.m.godfrey@gmail.com>
2022-05-25 15:09:47 +0200

    Finished routing and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: %I[index show new create] do
+    resources :reviews, only: %I[new create]
+  end
 end
