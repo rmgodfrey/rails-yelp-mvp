8ffc7d3ad94a572d0edc7147019ecf064eb4800a
Ross Godfrey <ross.m.godfrey@gmail.com>
2022-05-25 14:15:23 +0200

    Seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+NUMBER_OF_RESTAURANTS = 5
+MAX_REVIEWS_PER_RESTAURANT = 10
+
+def create_random_restaurant
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORIES.sample
+  )
+  rand(MAX_REVIEWS_PER_RESTAURANT + 1).times do
+    create_random_review(restaurant)
+  end
+end
+
+def create_random_review(restaurant)
+  Review.create(
+    content: Faker::Lorem.paragraph,
+    rating: rand(Review::MAX_RATING + 1),
+    restaurant: restaurant
+  )
+end
+
+puts 'Creating restaurants...'
+NUMBER_OF_RESTAURANTS.times { create_random_restaurant }
